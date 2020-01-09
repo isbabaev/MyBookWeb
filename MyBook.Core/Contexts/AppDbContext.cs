d00cc882ec7d77a05b35e2a1d2f35c01e5e3aca3
Islam Babaev <babaev@databriz.ru>
2020-01-09 23:16:48 +0300

    Добавил контекст и модели ef core

---
+++ MyBook.Core/Contexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBook.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBook.Core.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
